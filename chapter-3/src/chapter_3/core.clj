98d91d153cbd9b42010911093c2c1769294d1099
Saad Guessous <guessous_saad@hotmail.com>
2019-01-29 22:21:22 +0200

    added Ex 4

--- chapter-3/src/chapter_3/core.clj
+++ chapter-3/src/chapter_3/core.clj
@@ -1,14 +1,14 @@
 (ns chapter-3.core
   (:gen-class))
 
-;;; Ex 2
-;;; Write a function that takes a number and adds 100 to it.
+;; Ex 2
+;; Write a function that takes a number and adds 100 to it.
 (defn add100
   [num]
   (+ num 100))
 
-;;; Ex 3
-;;; Write a function, dec-maker, that works exactly like the function inc-maker except with subtraction:
+;; Ex 3
+;; Write a function, dec-maker, that works exactly like the function inc-maker except with subtraction:
 (defn inc-maker
   "Create a custom incrementor"
   [inc-by]
@@ -23,3 +23,9 @@
 
 (def dec9 (dec-maker 9))
 
+;; Ex 4
+;; Write a function, mapset, that works like map except the return value is a set:
+(defn mapset
+  [f vec]
+  (set 
+   (map f vec)))
