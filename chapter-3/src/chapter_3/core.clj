b888eae866232c89dc2ce5c11ba504c290a5728c
Saad Guessous <guessous_saad@hotmail.com>
2019-01-29 22:13:43 +0200

    added Ex 3

--- chapter-3/src/chapter_3/core.clj
+++ chapter-3/src/chapter_3/core.clj
@@ -7,4 +7,19 @@
   [num]
   (+ num 100))
 
+;;; Ex 3
+;;; Write a function, dec-maker, that works exactly like the function inc-maker except with subtraction:
+(defn inc-maker
+  "Create a custom incrementor"
+  [inc-by]
+  #(+ % inc-by))
+
+(def inc3 (inc-maker 3))
+
+(defn dec-maker
+  "Create a custom decrementor"
+  [dec-by]
+  #(- % dec-by))
+
+(def dec9 (dec-maker 9))
 
